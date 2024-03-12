27a3a50309b609cfe58c9a11d47e1e923c052eb3
tajabar <121495180+tajabar@users.noreply.github.com>
2024-03-12 15:32:36 +0300

    Update __init__.py

--- lang/__init__.py
+++ lang/__init__.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/lang/__init__.py"
+file_name = "lang/__init__.py"
 
 from configs.config import settings
 
