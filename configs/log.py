82308b1621fe3e9fa9998754729a0e92c9f70302
tajabar <121495180+tajabar@users.noreply.github.com>
2024-03-12 14:46:50 +0300

    Update log.py

--- configs/log.py
+++ configs/log.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/configs/log.py"
+file_name = "configs/log.py"
 
 import os
 import asyncio
