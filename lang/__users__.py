a0d00d456e7de3da710f28221a66be3f82e2b73a
tajabar <121495180+tajabar@users.noreply.github.com>
2024-03-12 15:32:51 +0300

    Update __users__.py

--- lang/__users__.py
+++ lang/__users__.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/lang/__users__.py"
+file_name = "lang/__users__.py"
 
 # Default User Lang Data:
 
