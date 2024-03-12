422ccbc1283bf2995ae10f21ba870622afeca0e2
tajabar <121495180+tajabar@users.noreply.github.com>
2024-03-12 14:46:19 +0300

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # Copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/configs/config.py"
+file_name = "configs/config.py"
 
 import os
 
