16f659b515d6ae01ca9085cb13d07f4f95dc5508
tajabar <121495180+tajabar@users.noreply.github.com>
2024-03-12 14:44:50 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/logger.py"
+file_name = "logger.py"
 
 import logging
 logger = logging.getLogger(__name__)
